645de1273d9e694ebd264f1362d7925b2950f190
Adam Boyd <adamboyd50@gmail.com>
2015-09-24 15:02:58 -0700

    Initial Commit

---
+++ I Want Coffee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  I Want Coffee
+//
+//  Created by Adam on 2015-09-24.
+//  Copyright © 2015 Adam Boyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ I Want CoffeeTests/I_Want_CoffeeTests.m
@@ -0,0 +1,39 @@
+//
+//  I_Want_CoffeeTests.m
+//  I Want CoffeeTests
+//
+//  Created by Adam on 2015-09-24.
+//  Copyright © 2015 Adam Boyd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface I_Want_CoffeeTests : XCTestCase
+
+@end
+
+@implementation I_Want_CoffeeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
