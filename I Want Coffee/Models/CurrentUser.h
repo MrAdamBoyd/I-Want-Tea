1c049af916e9cd00dd61f2ab1ac3ac542edb0cde
Adam Boyd <adamboyd50@gmail.com>
2015-09-24 15:50:15 -0700

    current user model created

---
+++ I Want Coffee/Models/CurrentUser.h
@@ -0,0 +1,13 @@
+//
+//  CurrentUser.h
+//  I Want Coffee
+//
+//  Created by Adam on 2015-09-24.
+//  Copyright © 2015 Adam Boyd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CurrentUser : NSObject
+
+@end
---
+++ I Want Coffee/Models/CurrentUser.m
@@ -0,0 +1,13 @@
+//
+//  CurrentUser.m
+//  I Want Coffee
+//
+//  Created by Adam on 2015-09-24.
+//  Copyright © 2015 Adam Boyd. All rights reserved.
+//
+
+#import "CurrentUser.h"
+
+@implementation CurrentUser
+
+@end
